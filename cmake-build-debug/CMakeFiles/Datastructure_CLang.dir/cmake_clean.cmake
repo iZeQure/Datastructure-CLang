c6180286aa861037765bd0918fce7aeca56058b4
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-10-13 09:43:35 +0200

    Added base project files.

---
+++ cmake-build-debug/CMakeFiles/Datastructure_CLang.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Datastructure_CLang.dir/main.c.obj"
+  "Datastructure_CLang.exe"
+  "Datastructure_CLang.exe.manifest"
+  "Datastructure_CLang.lib"
+  "Datastructure_CLang.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Datastructure_CLang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
